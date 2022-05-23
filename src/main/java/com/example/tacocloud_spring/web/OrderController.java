0dde3813692ec28076c93844944f354046f4936b
decam2009 <KaloshinBA@gmail.com>
2022-05-23 11:53:03 +0300

    Added validator

--- src/main/java/com/example/tacocloud_spring/web/OrderController.java
+++ src/main/java/com/example/tacocloud_spring/web/OrderController.java
@@ -3,11 +3,14 @@ package com.example.tacocloud_spring.web;
 import com.example.tacocloud_spring.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -18,8 +21,12 @@ public class OrderController {
   public String orderForm() {
     return "orderForm";
   }
+
   @PostMapping
-  public String processOrder(TacoOrder tacoOrder, SessionStatus sessionStatus){
+  public String processOrder(@Valid TacoOrder tacoOrder, Errors errors, SessionStatus sessionStatus) {
+    if (errors.hasErrors()){
+      return "orderForm";
+    }
     log.info("Order submitted{}: ", tacoOrder);
     sessionStatus.setComplete();
     return "redirect:/";
