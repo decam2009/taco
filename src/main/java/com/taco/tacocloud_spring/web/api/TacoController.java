8ac2b7b4452ce2a602823fd1090b4cf12d3698e4
decam2009 <KaloshinBA@gmail.com>
2022-06-14 22:07:19 +0300

    fixed TacoController

--- src/main/java/com/taco/tacocloud_spring/web/api/TacoController.java
+++ src/main/java/com/taco/tacocloud_spring/web/api/TacoController.java
@@ -2,6 +2,8 @@ package com.taco.tacocloud_spring.web.api;
 
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 import com.taco.tacocloud_spring.Taco;
+import com.taco.tacocloud_spring.TacoOrder;
+import com.taco.tacocloud_spring.data.OrderRepository;
 import com.taco.tacocloud_spring.data.TacoRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -17,9 +19,11 @@ import java.util.Optional;
 public class TacoController {
 
   private TacoRepository tacoRep;
+  private OrderRepository orderRepo;
 
-  public TacoController(TacoRepository tacoRepository) {
-    this.tacoRep = tacoRepository;
+  public TacoController(TacoRepository tacoRep, OrderRepository orderRepo) {
+    this.tacoRep = tacoRep;
+    this.orderRepo = orderRepo;
   }
 
   @GetMapping(params = "recent")
@@ -37,10 +41,41 @@ public class TacoController {
     return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
   }
 
-  @PostMapping(consumes = "application/json")//Атрибут consumes определяет формат входных данных в запросе, а produces – формат возвращаемых клиенту данных.
+  @PostMapping(consumes = "application/json")
+//Атрибут consumes определяет формат входных данных в запросе, а produces – формат возвращаемых клиенту данных.
   @ResponseStatus(HttpStatus.CREATED)
   public Taco postTaco(@RequestBody Taco taco) {
     return tacoRep.save(taco);
+  }
+
+  @PatchMapping(path = "/{orderId}", consumes = "application/json")
+  public TacoOrder patchOrder(@PathVariable("orderId") Long orderId, @RequestBody TacoOrder patch) {
+    TacoOrder order = orderRepo.findById(orderId).get();
+    if (patch.getDeliveryName() != null) {
+      order.setDeliveryName(patch.getDeliveryName());
+    }
+    if (patch.getDeliveryStreet() != null) {
+      order.setDeliveryStreet(patch.getDeliveryStreet());
+    }
+    if (patch.getDeliveryCity() != null) {
+      order.setDeliveryCity(patch.getDeliveryCity());
+    }
+    if (patch.getDeliveryStreet() != null) {
+      order.setDeliveryStreet(patch.getDeliveryStreet());
+    }
+    if (patch.getDeliveryZip() != null) {
+      order.setDeliveryZip(patch.getDeliveryZip());
+    }
+    if (patch.getCcNumber() != null) {
+      order.setCcNumber(patch.getCcNumber());
+    }
+    if (patch.getCcExpiration() != null) {
+      order.setCcExpiration(patch.getCcExpiration());
+    }
+    if (patch.getCcCVV() != null) {
+      order.setCcCVV(patch.getCcCVV());
+    }
+    return orderRepo.save(order);
   }
 
 }
